46dce0bcdff9ea4dcd0af922a852c7aa4bce9688
hellopoisonx <x1665341912@gmail.com>
2023-05-18 22:57:14 +0800

    nothing

--- lua/general/lazy.lua
+++ lua/general/lazy.lua
@@ -7,7 +7,7 @@ if not vim.loop.fs_stat(lazypath) then
     })
 end
 vim.opt.rtp:prepend(lazypath)
-
+--
 require("lazy").setup({
     spec = {{import = "plugins"}},
     default = {lazy = false, version = true},
